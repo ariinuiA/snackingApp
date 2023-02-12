c867003909be1e1b43550bf359243e3e18dc3346
Gabe <ariinuitahiti@gmail.com>
2023-02-11 18:21:40 -1000

    Création de LoginScreen et HomeScreen
    Le LoginScreen et le HomeScreen sont en cours.

--- AV_projet/codingSnack/App.tsx
+++ AV_projet/codingSnack/App.tsx
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './assets/screens/LoginScreen';
+import HomeScreen from './assets/screens/HomeScreen';
 
 export default function App() {
+  console.log("Application executed");
+  
+
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
